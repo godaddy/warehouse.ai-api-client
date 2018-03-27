53464b310e20bac464ee5eb546ddb67812d45014
msluther <msluther99@gmail.com>
2018-03-27 13:58:15 -0700

    [fix] Pass the _right_ set of options to builds ctor (#4)

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,8 @@
 const assume = require('assume');
+const sinon = require('sinon');
 const Warehouse = require('..');
+
+assume.use(require('assume-sinon'));
 
 describe('warehousei.ai-api-client.tests', function () {
   const wrhs = new Warehouse('https://my-warehouse-api');
@@ -16,5 +19,20 @@ describe('warehousei.ai-api-client.tests', function () {
 
   it('should have a builds prototype attached', function () {
     assume(wrhs.builds).is.instanceof(Warehouse.Builds);
+  });
+
+  it('should pass the right config to builds', function () {
+    const buildsSpy = sinon.spy(Warehouse, 'Builds');
+    const buildConfig = { foo: 'bar' };
+
+    const warehouseClient = new Warehouse({
+      uri: 'http://ImAUri',
+      builds: buildConfig,
+      nother: 'thing' });
+
+    assume(warehouseClient).is.not.null();
+    assume(buildsSpy).is.called(1);
+    assume(buildsSpy).is.calledWithNew();
+    assume(buildsSpy).is.calledWithExactly(warehouseClient, buildConfig);
   });
 });
